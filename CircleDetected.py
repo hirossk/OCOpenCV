1a3a5ea36d2a39c7651102b1f4f95e255fe4889a
Sasaki Hiroyuki <hsasaki@tipo33.org>
2021-06-22 06:05:12 +0900

    not run temporary commit

---
+++ CircleDetected.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+import numpy as np
+
+capture = cv2.VideoCapture(0)
+
+
+while(True):
+    ret, frame = capture.read()
+    window1 = (800, 600)
+    cimg = cv2.resize(frame,window1)
+#img = capture.read()
+    cimg = cv2.medianBlur(cimg,5)
+    #cimg = cv2.cvtColor(cimg,cv2.COLOR_GRAY2BGR)
+    circles = cv2.HoughCircles(cimg,cv2.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+    cv2.imshow('detected circles',cimg)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
